555bbbba302bb988fee2930fe259fc950f668125
Johannes Moe <johannesmoehrl@protonmail.com>
2023-02-12 07:25:39 +0100

    add env

--- server/api.js
+++ server/api.js
@@ -1,9 +1,13 @@
 let express = require('express');
+const req = require('express/lib/request');
 let mongoose = require('mongoose');
 let router = express.Router();
 let bottleModel = require('./waterbottleschema');
+require('dotenv').config();
 
-let uri = `mongodb+srv://jojom:<password>@cluster0.obzgbew.mongodb.net/?retryWrites=true&w=majority`;
+const user = process.env.USER_NAME;
+const pw = process.env.PASSWORD;
+let uri = `mongodb+srv://jojom:${pw}@cluster0.obzgbew.mongodb.net/?retryWrites=true&w=majority`;
 
 const db = uri;
 mongoose.Promise = global.Promise;
